04c1be7c4cf66e69ee96d122bc211f46f8bdd9b0
EC2 Default User <8884571+elliott-king@users.noreply.github.com>
2018-10-01 20:52:31 +0000

    Finalize pt 2

--- block.go
+++ block.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-    "bytes"
-    "crypto/sha256"
-    "strconv"
     "time"
 )
 
@@ -15,20 +12,17 @@ type Block struct {
     Data                []byte
     PrevBlockHash       []byte
     Hash                []byte
-}
-
-// Concatenate & calc SHA-256 on block fields. Oversimplification.
-func (b *Block) SetHash() {
-    timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-    headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-    hash := sha256.Sum256(headers)
-
-    b.Hash = hash[:]
+    Nonce               int
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-    block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-    block.SetHash()
+    block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+    pow := NewProofOfWork(block)
+    nonce, hash := pow.Run()
+
+    block.Hash = hash[:]
+    block.Nonce = nonce
+
     return block
 }
 
